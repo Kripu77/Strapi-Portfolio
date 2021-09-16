095340024659327b978473a5b1b8eb3829ae7d13
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-09-16 18:28:41 +1000

    Masthead.js

--- strapi-portfolio/src/App.js
+++ strapi-portfolio/src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import "./styles.css"
-import Nav from "./nav"
+import Nav from "./nav";
+import MastHead from "./masthead";
 let App = ()=>{
     return(
+        <div>
       <Nav/>
+      <MastHead/>
+      </div>
     )
 }
 export default App;
