1ab92bf807df7c3cb28b22f18683678dca336e0a
Kripu77 <84505567+Kripu77@users.noreply.github.com>
2021-10-18 14:14:45 +1100

    ityped effect working

--- strapi-portfolio/src/masthead.js
+++ strapi-portfolio/src/masthead.js
@@ -5,15 +5,18 @@ import {TiHeartFullOutline} from "react-icons/ti";
 import { init } from 'ityped'
 let MastHead = ()=>{
 
-//for fade in typing effect
+//for fade in typing effect using the ityped npm package
 const textRef = useRef();
-useEffect(()=>{}, {})
+useEffect(()=>{
+init(textRef.current,{ showCursor: false, strings:["Front-End Developer", "Web Designer", "Blogger", "Content Creator"]})
+    console.log(textRef)
+}, {})
 
     return(<header className="masthead  text-white text-center main-background" style={{backgroundColor:"#41b0bf"}}>
             <div className="container d-flex align-items-center flex-column">
                 {/* <!-- Masthead Avatar Image--> */}
                 <img className="masthead-avatar mb-5 bg-primary" src={plane} alt="..." />
-                <img className="masthead-avatar mb-5" src={avatar} alt="..." style={{height:"300px", width:"300px", borderRadius:"250px"}} />
+                <img className="masthead-avatar mb-5" src={avatar} alt="..." style={{height:"300px", width:"300px", borderRadius:"100%"}} />
                 {/* <!-- Masthead Heading--> */}
                 <img className="masthead-avatar mb-5 bg-primary" src={plane} alt="..." />
                 <h1 className="masthead-heading text-uppercase mb-0">Kripu Khadka</h1>
@@ -24,7 +27,8 @@ useEffect(()=>{}, {})
                     <div className="divider-custom-line"></div>
                 </div>
                 {/* <!-- Masthead Subheading--> */}
-                <p className="masthead-subheading font-weight-light mb-0">Front-End Web Developer- Web Designer - Illustrator - Blogger</p>
+                <p className="masthead-subheading font-weight-light mb-0"
+                 ref={textRef}> </p>
                 <p className="main-detail"> Made with <span className="mainpara-icon"> <TiHeartFullOutline/>  </span> by Kripu Khadka </p>
             </div>
         </header>)
